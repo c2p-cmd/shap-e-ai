57432d2ec8d638f440b1dbbc8068bbf99a472aae
DIckson Pun <dickpun499@gmail.com>
2023-06-18 17:26:43 +0000

    Duplicate from hysts/Shap-E
    Co-authored-by: hysts <hysts@users.noreply.huggingface.co>

---
+++ README.md
@@ -0,0 +1,18 @@
+---
+title: Shap-E
+emoji: 🧢
+colorFrom: yellow
+colorTo: blue
+sdk: gradio
+sdk_version: 3.34.0
+python_version: 3.10.11
+app_file: app.py
+pinned: false
+license: mit
+suggested_hardware: t4-small
+duplicated_from: hysts/Shap-E
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+
+https://arxiv.org/abs/2305.02463
---
+++ app_image_to_3d.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import pathlib
+import shlex
+import subprocess
+
+import gradio as gr
+
+from model import Model
+from settings import CACHE_EXAMPLES, MAX_SEED
+from utils import randomize_seed_fn
+
+
+def create_demo(model: Model) -> gr.Blocks:
+    if not pathlib.Path('corgi.png').exists():
+        subprocess.run(
+            shlex.split(
+                'wget https://raw.githubusercontent.com/openai/shap-e/d99cedaea18e0989e340163dbaeb4b109fa9e8ec/shap_e/examples/example_data/corgi.png -O corgi.png'
+            ))
+    examples = ['corgi.png']
+
+    def process_example_fn(image_path: str) -> str:
+        return model.run_image(image_path)
+
+    with gr.Blocks() as demo:
+        with gr.Box():
+            image = gr.Image(label='Input image',
+                             show_label=False,
+                             type='filepath')
+            run_button = gr.Button('Run')
+            result = gr.Model3D(label='Result', show_label=False)
+            with gr.Accordion('Advanced options', open=False):
+                seed = gr.Slider(label='Seed',
+                                 minimum=0,
+                                 maximum=MAX_SEED,
+                                 step=1,
+                                 value=0)
+                randomize_seed = gr.Checkbox(label='Randomize seed',
+                                             value=True)
+                guidance_scale = gr.Slider(label='Guidance scale',
+                                           minimum=1,
+                                           maximum=20,
+                                           step=0.1,
+                                           value=3.0)
+                num_inference_steps = gr.Slider(
+                    label='Number of inference steps',
+                    minimum=1,
+                    maximum=100,
+                    step=1,
+                    value=64)
+
+        gr.Examples(examples=examples,
+                    inputs=image,
+                    outputs=result,
+                    fn=process_example_fn,
+                    cache_examples=CACHE_EXAMPLES)
+
+        inputs = [
+            image,
+            seed,
+            guidance_scale,
+            num_inference_steps,
+        ]
+
+        run_button.click(
+            fn=randomize_seed_fn,
+            inputs=[seed, randomize_seed],
+            outputs=seed,
+            queue=False,
+        ).then(
+            fn=model.run_image,
+            inputs=inputs,
+            outputs=result,
+            api_name='image-to-3d',
+        )
+    return demo
